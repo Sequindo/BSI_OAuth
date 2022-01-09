898fdda31ed8dbf95856506e504cdee90cdf217b
Kacper Nowak <kacpernowak@pop-os.localdomain>
2022-01-09 01:35:02 +0100

    Refactor application code based on google tutorial
    Signed-off-by: Kacper Nowak <kacpernowak@pop-os.localdomain>

--- app.py
+++ app.py
@@ -1,50 +1,124 @@
-from flask import Flask, session
-from authlib.integrations.flask_client import OAuth
-from flask.helpers import url_for
+# -*- coding: utf-8 -*-
+
+import os
+import flask
+import requests
+
+import google.oauth2.credentials
+import google_auth_oauthlib.flow
+import googleapiclient.discovery
 from werkzeug.utils import redirect
 
-app = Flask(__name__)
-app.secret_key = 'random_secret_key'
+#Handling the client credentials in accordance with the best practice (eg. JSON file, as opposed to hardcoding in plain text) - 1 point
+CLIENT_SECRETS_FILE = "client_secret.json"
 
-#oauth config
-oauth = OAuth(app)
-google = oauth.register(
-    name='google',
-    client_id='824282840327-4d9etrejbf10hm5je1qj3rmim3gsnogu.apps.googleusercontent.com',
-    client_secret='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',
-    access_token_url='https://accounts.google.com/o/oauth2/token',
-    access_token_params=None,
-    authorize_url='https://accounts.google.com/o/oauth2/auth',
-    authorize_params=None,
-    api_base_url='https://www.googleapis.com/oauth2/v1/',
-    userinfo_endpoint='https://openidconnect.googleapis.com/v1/userinfo',  # This is only needed if using openId to fetch user info
-    client_kwargs={'scope': 'openid email profile phone address'},
-)
+# using incremental authorization
+SCOPES = ['https://www.googleapis.com/auth/drive.metadata.readonly']
 
+app = flask.Flask(__name__)
+# Based on https://flask.palletsprojects.com/quickstart/#sessions.
+app.secret_key = 'cc1ea1e24cfe1913c32d9d17252ec97f263024fa13f16065d6eea1cad3215f1d'
 
 @app.route('/')
-def hello_world():
-    email = dict(session).get('email', None)
+def index():
+  userInfo = dict(flask.session).get('userinfo', None)
+  if(userInfo==None):
+    return f'Hello, None!'
+  else:
+    email = userInfo.get['email']
     return f'Hello, {email}!'
 
 @app.route('/login')
 def login():
-    _google = oauth.create_client('google')
-    redirect_uri = url_for('authorize', _external=True)
-    return _google.authorize_redirect(redirect_uri)
+    redirect_uri = flask.url_for('authorize', _external=True)
+    return flask.redirect(redirect_uri)
 
 @app.route('/authorize')
 def authorize():
-    _google = oauth.create_client('google')
-    token = _google.authorize_access_token()
-    resp = _google.get('userinfo', token=token)
-    userInfo = resp.json()
-    #do something with token, and profile
-    session['email'] = userInfo['email']
-    return redirect('/')
+  flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
+      CLIENT_SECRETS_FILE, scopes=SCOPES)
 
-@app.route('/logout')
-def logout():
-    for key in list(session.keys()):
-        session.pop(key)
-    return redirect('/')
+  flow.redirect_uri = flask.url_for('oauth2callback', _external=True)
+
+  authorization_url, state = flow.authorization_url(
+      access_type='offline',
+      include_granted_scopes='true')
+
+  flask.session['state'] = state
+
+  return flask.redirect(authorization_url)
+
+@app.route('/oauth2callback')
+def oauth2callback():
+  state = flask.session['state']
+
+  flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
+      CLIENT_SECRETS_FILE, scopes=SCOPES, state=state)
+  flow.redirect_uri = flask.url_for('oauth2callback', _external=True)
+
+  authorization_response = flask.request.url
+  flow.fetch_token(authorization_response=authorization_response)
+
+  credentials = flow.credentials
+  flask.session['credentials'] = credentials_to_dict(credentials)
+  return flask.redirect(flask.url_for('getInfo'))
+
+@app.route('/getInfo')
+def getInfo():
+  if 'credentials' not in flask.session:
+    return flask.redirect('authorize')
+
+  credentials = google.oauth2.credentials.Credentials(
+      **flask.session['credentials'])
+
+  user_info_service = googleapiclient.discovery.build(
+      serviceName='oauth2', version='v2', credentials=credentials)
+
+  user_info = user_info_service.userinfo().get().execute()
+  flask.session['userinfo'] = user_info
+  return flask.redirect('/')
+
+
+@app.route('/revoke')
+def revoke():
+  if 'credentials' not in flask.session:
+    return ('You need to authorize before ' +
+            'testing the code to revoke credentials.')
+
+  credentials = google.oauth2.credentials.Credentials(
+    **flask.session['credentials'])
+
+  revoke = requests.post('https://oauth2.googleapis.com/revoke',
+      params={'token': credentials.token},
+      headers = {'content-type': 'application/x-www-form-urlencoded'})
+
+  status_code = getattr(revoke, 'status_code')
+  if status_code == 200:
+    return('Credentials successfully revoked.' + redirect('/'))
+  else:
+    return('An error occurred.' + redirect('/'))
+
+
+@app.route('/clear')
+def clear_credentials():
+  if 'credentials' in flask.session:
+    del flask.session['credentials']
+  return ('Credentials have been cleared.<br><br>' +
+          redirect('/'))
+
+
+def credentials_to_dict(credentials):
+  return {'token': credentials.token,
+          'refresh_token': credentials.refresh_token,
+          'token_uri': credentials.token_uri,
+          'client_id': credentials.client_id,
+          'client_secret': credentials.client_secret,
+          'scopes': credentials.scopes}
+
+if __name__ == '__main__':
+  #allow insecure http
+  os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
+  #allow incremental authorization
+  os.environ['OAUTHLIB_RELAX_TOKEN_SCOPE'] = '1'
+
+  app.run('localhost', 5000, debug=True)
